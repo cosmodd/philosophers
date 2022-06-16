4a51876c339892f2526c1896aa4959fc877854c6
mrattez <mrattez@student.42nice.fr>
2022-06-16 10:30:09 +0200

    🚚 Changed error.c to logging.c and added warnings

--- philo/srcs/logging.c
+++ philo/srcs/logging.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   error.c                                            :+:      :+:    :+:   */
+/*   logging.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrattez <mrattez@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 10:56:13 by mrattez           #+#    #+#             */
-/*   Updated: 2022/06/13 15:47:00 by mrattez          ###   ########.fr       */
+/*   Updated: 2022/06/16 10:22:55 by mrattez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,11 @@ void	print_usage(void)
 	printf("\e[34;1;3m<eat_time> \e[0m");
 	printf("\e[36;1;3m<sleep_time> \e[0m");
 	printf("\e[90;3m[eat_count] \e[0m");
-	printf("\n\n \e[90;3mEach arguments should only contains digits.\e[0m\n\n");
+	printf("\n\n");
+	printf(" \e[90;3mEach arguments should only contains digits.\e[0m\n");
+	printf(" \e[90;3m<arg> -> required argument.\e[0m\n");
+	printf(" \e[90;3m[arg] -> optional argument.\e[0m\n");
+	printf("\n");
 }
 
 /**
@@ -35,4 +39,9 @@ void	print_usage(void)
 void	print_error(char *error)
 {
 	printf("\e[37;41;1;3m ERROR \e[0m\e[37;3m %s \e[0m\n", error);
+}
+
+void	print_warn(char *warn)
+{
+	printf("\e[37;43;1;3m WARNING \e[0m\e[37;3m %s \e[0m\n", warn);
 }
